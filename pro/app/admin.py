ce96a224727ec98568d6b274eaee241776870ee8
Amalendu <amalendhuak05@gmail.com>
2025-10-10 16:11:43 +0530

    Commit files

---
+++ pro/app/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import user
+from .models import item
+from .models import cart
+from .models import order
+from .models import PasswordReset
+
+admin.site.register(user)
+admin.site.register(item)
+admin.site.register(cart)
+admin.site.register(order)
+admin.site.register(PasswordReset)
