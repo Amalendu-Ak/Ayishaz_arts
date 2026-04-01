4b9a9803a993e5294c40bd6f96cc34bb3b322e0d
Amalendu <amalendhuak05@gmail.com>
2026-04-01 19:06:01 +0530

    Fix app import path

--- pro/pro/wsgi.py
+++ pro/pro/wsgi.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/Amalendu/Ayishaz_arts/pro'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
