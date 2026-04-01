476de71a5a407297ee880f4f39c5435878308e07
Amalendu <amalendhuak05@gmail.com>
2026-04-01 15:48:46 +0530

    Deploy ready

--- pro/app/admin.py
+++ pro/app/admin.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from django.contrib import admin
 
 from .models import user
@@ -12,18 +12,6 @@ admin.site.register(item)
 admin.site.register(cart)
 admin.site.register(order)
 admin.site.register(PasswordReset)
-=======
-from django.contrib import admin
 
-from .models import user
-from .models import item
-from .models import cart
-from .models import order
-from .models import PasswordReset
 
-admin.site.register(user)
-admin.site.register(item)
-admin.site.register(cart)
-admin.site.register(order)
-admin.site.register(PasswordReset)
->>>>>>> 855c29bce681cecb57ed5e27fb8e74abeff628d4
+
--- pro/pro/wsgi.py
+++ pro/pro/wsgi.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """
 WSGI config for pro project.
 
@@ -15,21 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for pro project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro.settings')
-
-application = get_wsgi_application()
->>>>>>> 855c29bce681cecb57ed5e27fb8e74abeff628d4
